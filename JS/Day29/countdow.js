9e39c2cb2fa5441c04239aa23d2b350654245d1e
HuyDo <huydo041203@gmail.com>
2025-01-17 09:24:55 +0700

    day 30

--- JS/Day29/countdow.js
+++ JS/Day29/countdow.js
@@ -1,31 +1,49 @@
 
-const countDate = new Date('January 1, 2026 00:00:00').getTime();
+// const countDate = new Date('January 1, 2026 00:00:00').getTime();
 
-function newYear() {
-    const now = new Date().getTime(); 
-    const gap = countDate - now; 
+// function newYear() {
+//     const now = new Date().getTime(); 
+//     const gap = countDate - now; 
 
-    const second = 1000; 
-    const minute = second * 60; 
-    const hour = minute * 60; 
-    const day = hour * 24; 
+//     const second = 1000; 
+//     const minute = second * 60; 
+//     const hour = minute * 60; 
+//     const day = hour * 24; 
+   
+//     const remainDay = Math.floor(gap / day); 
+//     const remainHours = Math.floor((gap % day) / hour);
+//     const remainMinute = Math.floor((gap % hour) / minute); 
+//     const remainSeconds = Math.floor((gap % minute) / second); 
 
    
-    const remainDay = Math.floor(gap / day); 
-    const remainHours = Math.floor((gap % day) / hour);
-    const remainMinute = Math.floor((gap % hour) / minute); 
-    const remainSeconds = Math.floor((gap % minute) / second); 
 
-    if (gap <= 0) {
-        document.getElementById('countdown').innerHTML = "Chúc mừng năm mới 2026...";
-        clearInterval(setInterval (newYear,1000)); // Stop setInterval
-        return;
-    }
+//     // phần dư của ngày sẽ là giờ
+//     // phần dư của giờ sẽ là phút
+//     // phần dư của phút sẽ là giây
 
-    document.getElementById('countdown').innerHTML = 
-        `Còn ${remainDay} ngày ${remainHours} giờ ${remainMinute} phút ${remainSeconds} giây là đến Tết 2026...`;
+//     if (gap <= 0) {
+//         document.getElementById('countdown').innerHTML = "Chúc mừng năm mới 2026...";
+//         clearInterval(setInterval (newYear,1000)); // Stop setInterval
+//         return;
+//     }
+
+//     document.getElementById('countdown').innerHTML = 
+//         `Còn ${remainDay} ngày ${remainHours} giờ ${remainMinute} phút ${remainSeconds} giây là đến Tết 2026...`;
+// }
+
+// // gọi làm hàm newYear mỗi s
+// setInterval (newYear,1000);
+
+
+const countDate = new Date('January 1, 2026 00:00:00').getTime();
+
+function countdown () {
+    const now = Date.now();
+    const gap = countDate - now;
+
+    const day = Math.floor(gap / 1000/ 60 / 60/ 24)
+    const hours = Math.floor(day * 24 / 60 )
+    console.log(gap % day / 60)
 }
 
-// gọi làm hàm newYear mỗi s
-setInterval (newYear,1000);
-
+countdown()
---
+++ JS/Day30/ex04.js
@@ -0,0 +1,99 @@
+const cart = [
+    {
+      id: 1,
+      name: 'T-Shirt',
+      price: 100000,
+      quantity: 2,
+      hotSale: false
+    },
+    {
+      id: 2,
+      name: 'Jean',
+      price: 200000,
+      quantity: 1,
+      hotSale: false
+    },
+    {
+      id: 3,
+      name: 'Skirt',
+      price: 150000,
+      quantity: 3,
+      hotSale: true
+    },
+    {
+      id: 4,
+      name: 'Shirt',
+      price: 120000,
+      quantity: 2,
+      hotSale: false
+    },
+    {
+      id: 5,
+      name: 'Jacket',
+      price: 250000,
+      quantity: 1,
+      hotSale: true
+    }
+  ];
+  
+let content = "";
+cart.forEach((item) => {
+    let style = "";
+    if (item.hotSale) {
+        style = 'style="color: red;"';
+    }
+    const itemElement = `
+        <tr ${style}>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td>${item.quantity}</td>
+            <td>${item.price * item.quantity}</td>
+        </tr>
+    `
+    content += itemElement;
+})
+
+console.log(content)
+
+const totalAmount = cart.reduce((acc, cur) => {
+    acc += cur.quantity * cur.price;
+    return acc;
+},0)
+
+const tfoot = `
+     <tr>
+      <td colspan="3">Tổng tiền</td>
+      <td>${totalAmount}</td>
+    </tr>
+`
+
+document.body.innerHTML = `
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+    <table>
+        <thead>
+            <tr>
+                <th>Tên sản phẩm</th>
+                <th>Đơn giá</th>
+                <th>Số lượng</th>
+                <th>Thành tiền</th>
+            </tr>
+        </thead>
+        <tbody>
+            ${content}
+        </tbody>
+        <tfoot>
+            ${tfoot}
+        </tfoot>
+    </table>
+`
